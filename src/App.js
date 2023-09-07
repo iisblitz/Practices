04fd8406a8f75f57ac2bb2db5cd57cbbddeb4b26
Personal Blitz <iis_dbga@live.com>
2023-09-07 17:39:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import List from "./List";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <List/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <div>
+        <ul>
+            <li>Checking</li>
+        </ul>
+    </div>
+  )
+}
